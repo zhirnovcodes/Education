dfbc9418106abdec38c6f92f0b8a0f1f209fa401
Ivan Zhirnov <zhirnov.works@gmail.com>
2022-09-07 19:22:13 +0400

    graphs are drawn

--- Assets/Scripts/TextureDrawing/GraphDrawer.cs
+++ Assets/Scripts/TextureDrawing/GraphDrawer.cs
@@ -1,23 +1,189 @@
-using System.Collections;
-using System.Collections.Generic;
+using System;
 using UnityEngine;
 
-public class GraphDrawer
+public class GraphDrawer : IDisposable
 {
-    /*
-    public RenderTexture Texture
+    private ComputeShader _shader;
+    private readonly RenderTexture _valuesTexture;
+
+    private int _initResultKernelIndex;
+    private int _initValuesKernelIndex;
+    private int _paintKernelIndex;
+    private int _addValueKernelIndex;
+
+    private const string ResultName = "Result";
+    private const string ResultSizeName = "ResultSize";
+    private const string ValuesName = "Values";
+    private const string MaxOffsetName = "MaxOffset";
+    private const string ValueSizeName = "ValuesSize";
+    private const string ValueNewName = "ValueNew";
+    private const string IndexNewName = "IndexNew";
+    private const string BckgColName = "BckgColor";
+    private const string LinesColName = "LinesCol";
+
+    private const string ShaderName = "EarDrumOffset";
+
+    public const int DefaultTextureWidth = 256;
+    public const int DefaultTextureHeight = 128;
+
+    public RenderTexture Texture { get; }
+
+    public GraphDrawer(int textureWidth = DefaultTextureWidth, int textureHeight = DefaultTextureHeight)
     {
-        get
+        Texture = new RenderTexture(textureWidth, textureHeight, 1);
+        Texture.filterMode = FilterMode.Point;
+        Texture.enableRandomWrite = true;
+        Texture.Create();
+
+        _valuesTexture = new RenderTexture(Texture.width, 1, 1);
+        _valuesTexture.filterMode = FilterMode.Point;
+        _valuesTexture.enableRandomWrite = true;
+        _valuesTexture.format = RenderTextureFormat.RFloat;
+        _valuesTexture.Create();
+
+        _shader = (ComputeShader)GameObject.Instantiate(Resources.Load(ShaderName));
+
+        _initResultKernelIndex = _shader.FindKernel("InitResult");
+        _initValuesKernelIndex = _shader.FindKernel("InitValues");
+        _paintKernelIndex = _shader.FindKernel("Paint");
+        _addValueKernelIndex = _shader.FindKernel("AddValue");
+
+        _shader.SetInt(ValueSizeName, _valuesTexture.width);
+        _shader.SetVector(ResultSizeName, new Vector2(Texture.width, Texture.height));
+
+        _shader.SetTexture(_addValueKernelIndex, ValuesName, _valuesTexture);
+        _shader.SetTexture(_paintKernelIndex, ValuesName, _valuesTexture);
+        _shader.SetTexture(_initValuesKernelIndex, ValuesName, _valuesTexture);
+
+        _shader.SetTexture(_paintKernelIndex, ResultName, Texture);
+        _shader.SetTexture(_initResultKernelIndex, ResultName, Texture);
+    }
+
+
+    public void Clear()
+    {
+        _shader.Dispatch(_initValuesKernelIndex, _valuesTexture.width / 8, 1, 1);
+        _shader.Dispatch(_initResultKernelIndex, Texture.width / 8, Texture.height / 8, 1);
+    }
+
+    public void AddValue(int x, float value)
+    {
+        _shader.SetFloat(ValueNewName, value);
+        _shader.SetInt(IndexNewName, x);
+        _shader.Dispatch(_addValueKernelIndex, _valuesTexture.width / 8, 1, 1);
+
+    }
+
+    public void Paint(Color bckgColor, Color dotsColor, float maxOffset)
+    {
+        maxOffset = GraphController.MaxOffset ?? maxOffset;
+        _shader.SetFloat(MaxOffsetName, maxOffset);
+
+        _shader.SetVector(BckgColName, bckgColor);
+        _shader.SetVector(LinesColName, dotsColor);
+        _shader.Dispatch(_paintKernelIndex, Texture.width / 8, Texture.height / 8, 1);
+    }
+
+    public void Dispose()
+    {
+        Texture.Release();
+        if (_valuesTexture != null)
         {
-            if (_mainTexture == null)
-            {
-                _mainTexture = new RenderTexture(256, 128, 1);
-                _mainTexture.filterMode = FilterMode.Point;
-                _mainTexture.enableRandomWrite = true;
-                _mainTexture.Create();
-            }
-            return _mainTexture;
+            _valuesTexture.Release();
+        }
+
+        if (_shader != null)
+        {
+            UnityEngine.Object.Destroy(_shader);
         }
     }
-    */
 }
+
+/*
+private Queue<PeakValley> _peaks = new Queue<PeakValley>(10);
+private Queue<PeakValley> _valleys = new Queue<PeakValley>(10);
+private float _minVal = float.MaxValue;
+private float _maxVal = float.MinValue;
+*/
+
+
+/*
+private float CalcMax(float value, int index, int indexMin)
+{
+    var delta = value - _lastValue;
+
+    var wasChanged = false;
+
+    if (delta < 0)
+    {
+        _peaks.Enqueue(new PeakValley { Value = _lastValue, Index = index - 1 });
+        wasChanged = true;
+    }
+
+    while (_peaks.Count > 0 && _peaks.Peek().Index < indexMin)
+    {
+        _peaks.Dequeue();
+        wasChanged = true;
+    }
+
+    if (wasChanged)
+    {
+        var max = float.MinValue;
+        foreach (var p in _peaks)
+        {
+            max = Mathf.Max(max, p.Value);
+        }
+
+        _maxVal = max;
+    }
+    else
+    {
+        _maxVal = Mathf.Max(_maxVal, value);
+    }
+    _maxVal = Mathf.Max(_maxVal, value);
+    return _maxVal;
+}
+
+private float CalcMin(float value, int index, int indexMin)
+{
+    var delta = value - _lastValue;
+
+    var wasChanged = false;
+
+    if (delta > 0)
+    {
+        _valleys.Enqueue(new PeakValley { Value = _lastValue, Index = index });
+        wasChanged = true;
+    }
+
+    while (_valleys.Count > 0 && _valleys.Peek().Index < indexMin)
+    {
+        _valleys.Dequeue();
+        wasChanged = true;
+    }
+
+
+    if (wasChanged)
+    {
+        var min = float.MaxValue;
+        foreach (var v in _valleys)
+        {
+            min = Mathf.Min(min, v.Value);
+        }
+        _minVal = min;
+    }
+    else
+    {
+        _minVal = Mathf.Min(_minVal, value);
+    }
+
+    _minVal = Mathf.Min(_minVal, value);
+    return _maxVal;
+}
+
+private struct PeakValley
+{
+    public float Value;
+    public int Index;
+}
+*/
--- Assets/Scripts/TextureDrawing/PositionOffsetDrawer.cs
+++ Assets/Scripts/TextureDrawing/PositionOffsetDrawer.cs
@@ -3,42 +3,25 @@ using UnityEngine;
 
 public class PositionOffsetDrawer : MonoBehaviour, IDisposable
 {
-    private const string ResultName = "Result";
-    private const string ResultSizeName = "ResultSize";
-    private const string ValuesName = "Values";
-    private const string MaxOffsetName = "MaxOffset";
-    private const string ValueSizeName = "ValuesSize";
-    private const string ValueNewName = "ValueNew";
-    private const string IndexNewName = "IndexNew";
-
-    private const string BckgColName = "BckgColor";
-    private const string LinesColName = "LinesCol";
 
     [SerializeField] private Transform _target;
-    [SerializeField] private ComputeShader _shader;
     [SerializeField, Range(0.001f, 2f)] private float _analysisDeltaSec = 0.1f;
     [SerializeField, Range(0.01f, 7)] private float _maxOffset = 1f;
-    [SerializeField] private RenderTexture _valuesTexture;
 
     [SerializeField] private Color _bckgColor = new Color(0,0,0,0);
     [SerializeField] private Color _linesColor = new Color(1,1,1,1);
 
-    private int _initResultKernelIndex;
-    private int _initValuesKernelIndex;
-    private int _paintKernelIndex;
-    private int _addValueKernelIndex;
+    [SerializeField] private int _textureWidth = GraphDrawer.DefaultTextureWidth;
+    [SerializeField] private int _textureHeight = GraphDrawer.DefaultTextureHeight;
+
+    private GraphDrawer _drawer;
 
     private float _timeWithoutPainting;
     private int _lastValuesIndex;
     private Vector3 _stablePos;
-    private RenderTexture _mainTexture;
 
-    /*
-    private Queue<PeakValley> _peaks = new Queue<PeakValley>(10);
-    private Queue<PeakValley> _valleys = new Queue<PeakValley>(10);
-    private float _minVal = float.MaxValue;
-    private float _maxVal = float.MinValue;
-    */
+    public event Action<int, float> OnValueAdded;
+
     public Transform Target
     {
         set
@@ -49,24 +32,17 @@ public class PositionOffsetDrawer : MonoBehaviour, IDisposable
     }
 
     public RenderTexture Texture 
-    { 
-        get 
+    {
+        get
         {
-            if (_mainTexture == null)
-            {
-                _mainTexture = new RenderTexture(256, 128, 1);
-                _mainTexture.filterMode = FilterMode.Point;
-                _mainTexture.enableRandomWrite = true;
-                _mainTexture.Create();
-            }
-            return _mainTexture;
-        } 
+            _drawer = _drawer ?? new GraphDrawer(_textureWidth, _textureHeight);
+            return _drawer.Texture;
+        }
     }
 
     private void OnEnable()
     {
-        _shader.Dispatch(_initValuesKernelIndex, _valuesTexture.width / 8, 1, 1);
-        _shader.Dispatch(_initResultKernelIndex, Texture.width / 8, Texture.height / 8, 1);
+        _drawer.Clear();
 
         _lastValuesIndex = 0;
         _timeWithoutPainting = 0;
@@ -79,28 +55,7 @@ public class PositionOffsetDrawer : MonoBehaviour, IDisposable
             _stablePos = _target.position;
         }
 
-        _valuesTexture = new RenderTexture(256, 1, 1);
-        _valuesTexture.filterMode = FilterMode.Point;
-        _valuesTexture.enableRandomWrite = true;
-        _valuesTexture.format = RenderTextureFormat.RFloat;
-        _valuesTexture.Create();
-
-        _shader = (ComputeShader)Instantiate(Resources.Load(_shader.name));
-
-        _initResultKernelIndex = _shader.FindKernel("InitResult");
-        _initValuesKernelIndex = _shader.FindKernel("InitValues");
-        _paintKernelIndex = _shader.FindKernel("Paint");
-        _addValueKernelIndex = _shader.FindKernel("AddValue");
-
-        _shader.SetInt(ValueSizeName, _valuesTexture.width);
-        _shader.SetVector(ResultSizeName, new Vector2(Texture.width, Texture.height));
-
-        _shader.SetTexture(_addValueKernelIndex, ValuesName, _valuesTexture);
-        _shader.SetTexture(_paintKernelIndex, ValuesName, _valuesTexture);
-        _shader.SetTexture(_initValuesKernelIndex, ValuesName, _valuesTexture);
-
-        _shader.SetTexture(_paintKernelIndex, ResultName, Texture);
-        _shader.SetTexture(_initResultKernelIndex, ResultName, Texture);
+        _drawer = _drawer ?? new GraphDrawer(_textureWidth, _textureHeight);
     }
 
     void Update()
@@ -111,22 +66,16 @@ public class PositionOffsetDrawer : MonoBehaviour, IDisposable
         }
 
         var offset = (_target.position - _stablePos).magnitude * Mathf.Sign((_target.position - _stablePos).x);
-
+        
         if (_timeWithoutPainting >= _analysisDeltaSec)
         {
             _timeWithoutPainting = 0;
 
-            var timeIndex = Mathf.RoundToInt( Mathf.Clamp(_lastValuesIndex, 0, _valuesTexture.width));
 
-            _shader.SetVector(BckgColName, _bckgColor);
-            _shader.SetVector(LinesColName, _linesColor);
-
-            _shader.SetFloat(ValueNewName, offset);
-            var maxOffset = GraphController.MaxOffset ?? _maxOffset;
-            _shader.SetFloat(MaxOffsetName, maxOffset);
-            _shader.SetInt(IndexNewName, timeIndex);
-            _shader.Dispatch(_addValueKernelIndex, _valuesTexture.width / 8, 1, 1);
-            _shader.Dispatch(_paintKernelIndex, Texture.width / 8, Texture.height / 8, 1);
+            var timeIndex = Mathf.RoundToInt( Mathf.Clamp(_lastValuesIndex, 0, Texture.width));
+            var value = offset;
+            _drawer.AddValue(timeIndex, value);
+            _drawer.Paint(_bckgColor, _linesColor, _maxOffset);
 
             _lastValuesIndex++;
         }
@@ -139,96 +88,6 @@ public class PositionOffsetDrawer : MonoBehaviour, IDisposable
 
     public void Dispose()
     {
-        Texture.Release();
-        if ( _valuesTexture != null)
-        {
-            _valuesTexture.Release();
-        }
-
-        if (_shader != null)
-        {
-            Destroy(_shader);
-        }
+        _drawer.Dispose();
     }
-
-    /*
-    private float CalcMax(float value, int index, int indexMin)
-    {
-        var delta = value - _lastValue;
-
-        var wasChanged = false;
-
-        if (delta < 0)
-        {
-            _peaks.Enqueue(new PeakValley { Value = _lastValue, Index = index - 1 });
-            wasChanged = true;
-        }
-
-        while (_peaks.Count > 0 && _peaks.Peek().Index < indexMin)
-        {
-            _peaks.Dequeue();
-            wasChanged = true;
-        }
-
-        if (wasChanged)
-        {
-            var max = float.MinValue;
-            foreach (var p in _peaks)
-            {
-                max = Mathf.Max(max, p.Value);
-            }
-
-            _maxVal = max;
-        }
-        else
-        {
-            _maxVal = Mathf.Max(_maxVal, value);
-        }
-        _maxVal = Mathf.Max(_maxVal, value);
-        return _maxVal;
-    }
-
-    private float CalcMin(float value, int index, int indexMin)
-    {
-        var delta = value - _lastValue;
-
-        var wasChanged = false;
-
-        if (delta > 0)
-        {
-            _valleys.Enqueue(new PeakValley { Value = _lastValue, Index = index });
-            wasChanged = true;
-        }
-
-        while (_valleys.Count > 0 && _valleys.Peek().Index < indexMin)
-        {
-            _valleys.Dequeue();
-            wasChanged = true;
-        }
-
-
-        if (wasChanged)
-        {
-            var min = float.MaxValue;
-            foreach (var v in _valleys)
-            {
-                min = Mathf.Min(min, v.Value);
-            }
-            _minVal = min;
-        }
-        else
-        {
-            _minVal = Mathf.Min(_minVal, value);
-        }
-
-        _minVal = Mathf.Min(_minVal, value);
-        return _maxVal;
-    }
-
-    private struct PeakValley
-    {
-        public float Value;
-        public int Index;
-    }
-*/
 }
